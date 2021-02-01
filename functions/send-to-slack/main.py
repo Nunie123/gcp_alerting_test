ee40f086b1ed69059d66e38c72a373bb4c56a6f1
Ed Nunes <ed.nunes@zoro.com>
2021-01-31 21:45:35 -0500

    Update clud function

--- functions/send-to-slack/main.py
+++ functions/send-to-slack/main.py
@@ -1,17 +1,20 @@
 def send_to_slack(event, context):
-    webhook_url = get_secret('slack_webhook')
-    cloud_build_message = event.data
-    build_status = cloud_build_message.get('attributes').get('status')
-    build_id = cloud_build_message.get('attributes').get('buildId')
+    import requests
+    from google.cloud import secretmanager
+
+    client = secretmanager.SecretManagerServiceClient()
+    name = "projects/204024561480/secrets/slack_webhook/versions/latest"
+    response = client.access_secret_version(request={"name": name})
+    webhook_url = response.payload.data.decode("UTF-8")
+
+    print('printing event!!!!!!!!!!!!!!!!')
+    print(event)
+    print(event.get('attributes'))
+    build_status = event.get('attributes').get('status')
+    build_id = event.get('attributes').get('buildId')
     if build_status == 'SUCCESS':
         slack_text = f':large_green_circle: Cloud Build Success for {build_id}'
     else:
         slack_text = f':red_circle: ATTENTION! Cloud Build {build_id} did not succeed. Status is {build_status}.'
     slack_message = dict(text=slack_text)
     requests.post(webhook_url, json=slack_message)
-
-    def get_secret(secret_name, version="latest"):
-        secret_client = secretmanager.SecretManagerServiceClient()
-        secret_path = client.secret_version_path(project, secret_name, version)
-        secret = client.access_secret_version(secret_path)
-        return secret.payload.data.decode("UTF-8")
