ffabab84ab8de3835608d9e00c88a72682cf4ae4
Ed Nunes <ed.nunes@zoro.com>
2021-01-31 21:59:04 -0500

    Making unit test fail

--- dags/tests/test_python_scripts.py
+++ dags/tests/test_python_scripts.py
@@ -5,7 +5,7 @@ class TestScripts(unittest.TestCase):
 
     def test_script(self):
         self.assertTrue(1==1)
-        # self.assertTrue(1==0)
+        self.assertTrue(1==0)
 
 if __name__ == '__main__':
     unittest.main()
